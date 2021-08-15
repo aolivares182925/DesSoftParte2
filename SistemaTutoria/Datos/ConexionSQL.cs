900f7cb98405d07d3488e9322fc9a849676784ef
aolivares182925 <182925@unsaac.edu.pe>
2021-08-14 22:32:50 -0500

    Update ConexionSQL.cs

--- SistemaTutoria/Datos/ConexionSQL.cs
+++ SistemaTutoria/Datos/ConexionSQL.cs
@@ -11,7 +11,6 @@ namespace Datos
     public class ConexionSQL
     {
         SqlConnection conn = new SqlConnection("Data Source=tcp:serverproyectotutoria.database.windows.net;Initial Catalog=BD_Tutoria;User ID=admin1;Password=Grupo2DesarrolloSoftware");
-        //Data Source=tcp:serverproyectotutoria.database.windows.net;Initial Catalog=BD_Tutoria;User ID=admin1;Password=Grupo2DesarrolloSoftware;Application Name=MyApp
         //SqlConnection conn = new SqlConnection("Data Source=LAPTOP-IUT020T4;Initial Catalog=Tutoria;Integrated Security=True");
 
         public DataTable Select(string consulta)
