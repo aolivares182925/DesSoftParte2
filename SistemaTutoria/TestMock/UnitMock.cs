aba6ba2846d38e828952775414a2245d8cb8473a
Fiorella2919 <yo_fiorecita@hotmail.com>
2021-08-22 21:23:14 -0500

    Mock Agregar Alumno correcto

--- SistemaTutoria/TestMock/UnitMock.cs
+++ SistemaTutoria/TestMock/UnitMock.cs
@@ -31,6 +31,37 @@ namespace TestMock
         }
 
         [TestMethod]
+        public void TestMockAgregarAlumnoCorrecto()
+        {
+            Mock<ConectarSQL> Alumno = new Mock<ConectarSQL>();
+
+            //variables de prueba
+            string CodAlumno = "666666";
+            string Nombres = "SOYLA";
+            string APaterno = "BACA";
+            string AMaterno = "DEL CAMPO";
+            string Situacion = "EN RIESGO";
+            string CodTutor = "004";
+            string CodEscuela = "IN";
+
+
+            bool Expected = true;
+
+            //Indicamos el resultado del objeto, Que retorne una fila
+            Alumno.Setup(a => a.AgregarAlumnoSiPosible(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(),
+                It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+            //Alumno.Setup(a => a.Agregar(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), 
+            //    It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+            //Para obtener el resultado usamos .Object y lo almacenamos
+
+            bool actual = Alumno.Object.AgregarAlumnoSiPosible(CodAlumno, APaterno, AMaterno, Nombres, Situacion, CodTutor,
+                CodEscuela);
+            //Funcion Assert verifica si los resultados son iguales
+            Assert.AreEqual(actual, Expected);
+
+        }
+
+        [TestMethod]
         public void TestMockAgregarAlumnoIncorrecto()
         {
             Mock<ConectarSQL> Alumno = new Mock<ConectarSQL>();
