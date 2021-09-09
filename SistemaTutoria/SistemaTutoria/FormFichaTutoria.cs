756377c53a56c5d49d8403be3a7f5b58edfa120c
Fiorella2919 <yo_fiorecita@hotmail.com>
2021-09-08 23:40:01 -0500

    Ficha Tutoria
    Creación de la ficha de tutoría.

---
+++ SistemaTutoria/SistemaTutoria/FormFichaTutoria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaTutoria
+{
+    public partial class FormFichaTutoria : Form
+    {
+        public FormFichaTutoria()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            for (int i = Application.OpenForms.Count - 1; i >= 0; i--)
+            {
+                Application.OpenForms[i].Close();
+            }
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void lblApPaterno_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
