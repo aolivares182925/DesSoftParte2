beec3c4c2d25fd7cd1b8c59f92cc031bf64846bd
Fiorella2919 <yo_fiorecita@hotmail.com>
2021-09-10 22:53:54 -0500

    Funcionalidad Ficha Tutoria

--- SistemaTutoria/SistemaTutoria/FormFichaTutoria.cs
+++ SistemaTutoria/SistemaTutoria/FormFichaTutoria.cs
@@ -17,7 +17,54 @@ namespace SistemaTutoria
         {
             InitializeComponent();
             ConectarSQL conn = new ConectarSQL();
-            DataTable Dt = conn.SelectAlumnosDeTutor(CodTutor);
+            DataTable Dt = conn.SelectAlumno(CodAlumno);
+            tbCodigo.Text = Dt.Rows[0][0].ToString();
+            tbApPaterno.Text = Dt.Rows[0][1].ToString();
+            tbApMaterno.Text = Dt.Rows[0][2].ToString();
+            tbNombres.Text = Dt.Rows[0][3].ToString();
+            tbSituacion.Text = Dt.Rows[0][4].ToString();
+            tbCodEscuela.Text = Dt.Rows[0][6].ToString();
+
+            DataTable Dt_Sesion = conn.SelectFichaSesionAlumno(CodAlumno);
+            int Nro_Sesiones = Dt_Sesion.Rows.Count;
+            if (Nro_Sesiones != 0)
+            {
+                for (int i = 0; i < Nro_Sesiones; i++)
+                {
+                    int Nro_sesion = Int32.Parse(Dt_Sesion.Rows[i][0].ToString());
+
+                    switch (Nro_sesion)
+                    {
+                        case 1:
+                            cbTipo1.SelectedItem = Dt_Sesion.Rows[i][3].ToString();
+                            txtDescripcion1.Text = Dt_Sesion.Rows[i][3].ToString();
+                            txtReferencia1.Text = Dt_Sesion.Rows[i][3].ToString();
+                            txtObservaciones1.Text = Dt_Sesion.Rows[i][3].ToString();
+                            txtDescripcion1.Enabled = false;
+                            txtReferencia1.Enabled = false;
+                            txtObservaciones1.Enabled = false;
+                            break;
+
+                        case 2:
+                            txtDescripcion2.Text = Dt_Sesion.Rows[i][3].ToString();
+                            txtReferencia2.Text = Dt_Sesion.Rows[i][3].ToString();
+                            txtObservaciones2.Text = Dt_Sesion.Rows[i][3].ToString();
+                            txtDescripcion2.Enabled = false;
+                            txtReferencia2.Enabled = false;
+                            txtObservaciones2.Enabled = false;
+                            break;
+
+                        case 3:
+                            txtDescripcion3.Text = Dt_Sesion.Rows[i][3].ToString();
+                            txtReferencia3.Text = Dt_Sesion.Rows[i][3].ToString();
+                            txtObservaciones3.Text = Dt_Sesion.Rows[i][3].ToString();
+                            txtDescripcion3.Enabled = false;
+                            txtReferencia3.Enabled = false;
+                            txtObservaciones3.Enabled = false;
+                            break;
+                    }
+                }
+            }
         }
 
         private void btnClose_Click(object sender, EventArgs e)
@@ -48,6 +95,11 @@ namespace SistemaTutoria
                 ReleaseCapture();
                 SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
             }
+        }
+
+        private void label5_MouseDown(object sender, MouseEventArgs e)
+        {
+            panel1_MouseDown(sender, e);
         }
 
         #endregion mover ventana
